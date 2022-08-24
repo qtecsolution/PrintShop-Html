2b620f4267afb4fb02b1ce20d15958d69a7bc24c
it27427 <istiak.tushar.qtec@gmail.com>
2022-08-24 13:19:45 +0600

    contact completed with responsive

---
+++ assets/js/validations/contact.js
@@ -0,0 +1,67 @@
+const contactForm = document.querySelector("#contactform");
+const firstName = document.querySelector("#firstname");
+const lastName = document.querySelector("#lastname");
+const contactEmail = document.querySelector("#contactemail");
+const message = document.querySelector("#message");
+
+contactForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  handleInput();
+});
+
+function handleInput() {
+  // Values from dom elements ( input )
+  const firastNameValue = firstName.value.trim();
+  const lastNameValue = lastName.value.trim();
+  const emailValue = contactEmail.value.trim();
+  const messageValue = message.value.trim();
+
+  //  Checking for firstname
+  if (firastNameValue === '' || firastNameValue === null || firastNameValue === undefined) {
+    setErrorFor(firstName);
+  } else {
+    showSucces(firstName);
+  }
+
+  //  Checking for lastname
+  if (lastNameValue === '' || lastNameValue === null || lastNameValue === undefined) {
+    setErrorFor(lastName);
+  } else {
+    showSucces(lastName);
+  }
+
+  // Checking for email
+  if (emailValue === '' || emailValue === null || emailValue === undefined) {
+    setErrorFor(contactEmail);
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(contactEmail);
+  } else {
+    showSucces(contactEmail);
+  }
+
+  // Checking for message
+  if (messageValue === '' || messageValue === null || messageValue === undefined) {
+    setErrorFor(message);
+  } else {
+    showSucces(message);
+  }
+}
+
+// SET ERROR MESSAGE
+function setErrorFor(input) {
+  let inputbox = input.parentElement;
+  inputbox.classList.add('error');
+}
+
+// REMOVE ERROR MESSAGE
+function showSucces(input) {
+  let inputbox = input.parentElement;
+  inputbox.classList.remove('error');
+  inputbox.classList.add('success');
+}
+
+// To check if email is valid or not
+function isEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
